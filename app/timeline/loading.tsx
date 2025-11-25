0bbf1b72154fb51b5d7f950bc04f03eb488992ad
Ajith27887 <ajithyogesh.n@gmail.com>
2025-11-26 03:17:49 +0530

    loading component added

---
+++ app/timeline/loading.tsx
@@ -0,0 +1,5 @@
+import LoadingSkeleton from "../loading/LoadingSkeleton"
+
+export default function Loading() {
+	  return <LoadingSkeleton />
+}
--- app/timeline/page.tsx
+++ app/timeline/page.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Image from 'next/image'
+import TTY3 from "@/img/TTY3.jpg"
+import TTY1 from "@/img/TTY1.jpg"
+import TTY2 from "@/img/TTY2.jpg"
+
 
 const page = () => {
   return (
 	<div>
-	  <p>Timeline</p>
+	  	 <Image src={TTY2} className='w-fit h-fit' alt="demo"  />
+	  	 <Image src={TTY3} className='w-fit h-fit' alt="demo"  />
+	  	 <Image src={TTY1} className='w-fit h-fit' alt="demo"  />
+
 	</div>
   )
 }
