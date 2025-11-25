4fa9b58ee696e53612437795e542aa24d84bcadc
Ajith27887 <ajithyogesh.n@gmail.com>
2025-11-26 00:59:14 +0530

    first commit

---
+++ app/components/Main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+	<div className='p-5'> 
+	  <img src="./img/TTY1.jpg" className='w-10 h-full' alt="demo"  />
+	</div>
+  )
+}
+
+export default Main
