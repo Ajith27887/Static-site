31947ace09641541415729d709a393c47a550166
Ajith27887 <ajithyogesh.n@gmail.com>
2025-11-26 02:32:27 +0530

    home page added

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+"client"
+import Image from "next/image"
+import Link from "next/link"
+import TTY3 from "@/img/TTY3.jpg"
 
 const Main = () => {
   return (
-	<div className='p-5'> 
-	  <img src="./img/TTY1.jpg" className='w-10 h-full' alt="demo"  />
+	<div className='relative'> 
+	  <Image src={TTY3} className='w-fit h-fit' alt="demo"  />
+	  <div>
+		<Link href="/timeline" className="text-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white text-black p-3 w-48 text-2xl rounded-2xl" passHref>
+			Timeline
+		</Link>
+	  </div>
 	</div>
   )
 }
