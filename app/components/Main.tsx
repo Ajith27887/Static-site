6d863753488f5229e9394394aab2e0f1f1f1b0b1
Ajith27887 <ajithyogesh.n@gmail.com>
2025-12-07 18:50:32 +0530

    grid images added

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -1,4 +1,4 @@
-"client"
+"use client"
 import Image from "next/image"
 import Link from "next/link"
 import TTY3 from "@/img/TTY3.jpg"
@@ -6,9 +6,10 @@ import TTY3 from "@/img/TTY3.jpg"
 const Main = () => {
   return (
 	<div className='relative'> 
-	  <Image src={TTY3} className='w-fit h-fit' alt="demo"  />
-	  <div>
-		<Link href="/timeline" className="text-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white text-black p-3 w-48 text-2xl rounded-2xl" passHref>
+	  <Image src={TTY3} className='w-full h-full object-cover' alt="demo"  />
+	  <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+	  	<h1 className="  text-4xl font-bold text-white">TTY</h1>
+		<Link href="/timeline" className="mt-6 tracking-widest text-red-500 font-extrabold text-center bg-white hover:bg-gray-200 text-black p-3 w-48 text-2xl rounded-2xl" passHref>
 			Timeline
 		</Link>
 	  </div>
--- app/timeline/page.tsx
+++ app/timeline/page.tsx
@@ -1,17 +1,37 @@
-import React from 'react'
 import Image from 'next/image'
-import TTY3 from "@/img/TTY3.jpg"
-import TTY1 from "@/img/TTY1.jpg"
 import TTY2 from "@/img/TTY2.jpg"
+import TTY4 from "@/img/20190414_1350_SingaporeOpen2019_BPMR_2141.jpg"
+import TTY5 from "@/img/20190922_1448_ChinaOpen2019_BPYL8841.jpg"
+import TTY6 from "@/img/20191026_1712_FrenchOpen2019_BPYN2406.jpg"
 
 
 const page = () => {
   return (
-	<div>
-	  	 <Image src={TTY2} className='w-fit h-fit' alt="demo"  />
-	  	 <Image src={TTY3} className='w-fit h-fit' alt="demo"  />
-	  	 <Image src={TTY1} className='w-fit h-fit' alt="demo"  />
-
+	<div className='grid grid-cols-4 gap-0 bg-black p-5 h-dvh'>
+	  	 <div className='relative group w-full h-11/12'>
+			<Image src={TTY2} className='w-full h-full object-cover' alt="demo"  />
+			<div className='absolute inset-0 bg-black/50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300'>
+				<p className='text-white font-bold text-lg'>View</p>
+			</div>
+		 </div>
+		 <div className='relative group w-full h-11/12'>
+		 	<Image src={TTY5} className='w-full h-full object-cover' alt='demo' />
+			<div className='absolute inset-0 bg-black/50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300'>
+				<p className='text-white font-bold text-lg'>View</p>
+			</div>
+		 </div>
+ 		<div className='relative group w-full h-11/12'>
+			<Image src={TTY6} className='object-cover w-full h-full ' alt='demo' />
+			<div className='absolute inset-0 bg-black/50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300'>
+				<p className='text-white font-bold text-lg'>View</p>
+			</div>
+		</div>
+		<div className='relative group w-full h-11/12'>
+			<Image alt='demo' src={TTY4} className='w-full h-full object-cover' />
+			<div className='absolute inset-0 bg-black/50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300'>
+				<p className='text-white font-bold text-lg'>View</p>
+			</div>
+		</div>
 	</div>
   )
 }
